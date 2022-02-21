30076baa995502f2285766bd796be845c0d040e8
@cheeks cloud <agneskaswii700@gmail.com>
2022-02-21 19:00:42 +0300

    ft: tis is a game

---
+++ variation.js
@@ -0,0 +1,18 @@
+export function getNumberOfChars(name){
+  return name.length;
+}
+export function getFirstChar(name) {
+  return name[o]; //  index
+}
+export function getLastChar(name){
+  return name.CharAt(name.length -1);//used a method CharAt
+}
+export function getLower(name){
+  return name.toLowerCase();
+}
+export function getUpper(name){
+  return name.toUpperCase();
+}
+export function getCapitalized(name){
+  return name[0].toUpperCase() + name.subString(1).toLowerCase();
+}
