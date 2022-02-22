6c9189949d19b592325f9d3c3cd4ce909176a836
@cheeks cloud <agneskaswii700@gmail.com>
2022-02-22 12:48:57 +0300

    fix:fixed bugs

--- index.js
+++ index.js
@@ -7,43 +7,45 @@ const answer3 = document.querySelector("#answer3");
 const answer4 = document.querySelector("#answer4");
 const answer5 = document.querySelector("#answer5");
 const answer6 = document.querySelector("#answer6");
+const answer7 = document.querySelector("#answer7");
 
 const names = document.querySelector("#your-name");
 
 names.addEventListener("keyup",() => {
   const value = names.value;
 
+  function getNumberOfChars(value){
+    return value.length;
+  }
+   function getFirstChar(value) {
+    return value[0]; //  index
+  }
+  function getLastChar(value){
+    return value.length -1;
+  }
+  function getLower(value){
+    return value.toLowerCase();
+  }
+  function getUpper(value){
+    return value.toUpperCase();
+  }
+  function getCapitalized(value){
+    return value[0].toUpperCase() + value.slice(1).toLowerCase();
+  }
+  function getReplace(value){
+    return value.replace("value","repla");
+  }
+
   answer1.textContent = getNumberOfChars(value);
   answer2.textContent = getFirstChar(value);
   answer3.textContent = getLastChar(value);
   answer4.textContent = getLower(value);
   answer5.textContent = getUpper(value);
-
-  if(value) {
-    answer6.textContent = getCapitalized(value);
-
-  }else{
-    answer6.textContent = "";
-  }
+  answer6.textContent = getCapitalized(value);
+  answer7.innerHTML= getReplace(value);
+  
 
 });
 
-function getNumberOfChars(names){
-  return names.length;
-}
- function getFirstChar(names) {
-  return names[0]; //  index
-}
- function getLastChar(names){
-  return names.CharAt(names.length -1);//used a method CharAt
-}
-function getLower(names){
-  return names.toLowerCase();
-}
-function getUpper(names){
-  return names.toUpperCase();
-}
-function getCapitalized(names){
-  return names[0].toUpperCase() + names.subString(1).toLowerCase();
-}
+
 
